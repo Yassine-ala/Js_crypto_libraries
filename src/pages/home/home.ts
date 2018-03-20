e8ed19320be59df624aecec8bc33c328082ca994
yassine.alaoui <yassine.alaoui@sully-group.fr>
2018-03-20 13:49:07 +0100

    Node-Forge vs Crypto-Js

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import * as Forge  from 'node-forge';
+import * as CryptoJS from 'crypto-js';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,78 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  key = Forge.random.getBytesSync(32);
+  iv = Forge.random.getBytesSync(12);
+  text = "Un paragraphe est une section de texte en prose vouée au développement d'un point particulier souvent au moyen de plusieurs phrases, dans la continuité du précédent et du suivant. Sur le plan typographique, le début d'un paragraphe est marqué par un léger renfoncement (alinéa) ou par un saut de ligne.";
+  cryptJs: any;
+  cryptForge: any;
+
+  constructor() {}
+
+  cryptoJs(){
+
+    console.log("Generated Key : "+this.key);
+
+    this.cryptJs = CryptoJS.AES.encrypt(this.text, this.key.toString(), { iv: this.iv });
+    console.log("Crypted Msg : "+this.cryptJs.toString());
 
   }
 
+  cryptoForge(){
+    console.log("Generated key : "+this.key);
+
+    //enc
+    let cipher = Forge.cipher.createCipher('AES-CFB', this.key);
+    cipher.start({iv: this.iv});
+    cipher.update(Forge.util.createBuffer(this.text));
+    this.cryptForge = cipher.output;
+    console.log(this.cryptForge.toHex());
+  }
+
+  decryptoJs(){
+    let decrypt = CryptoJS.AES.decrypt(this.cryptJs.toString(), this.key.toString(), { iv: this.iv });
+    let msg = decrypt.toString(CryptoJS.enc.Utf8);
+    console.log(msg);
+  }
+
+  decryptoForge(){
+
+    let decipher = Forge.cipher.createDecipher('AES-CFB', this.key);
+    decipher.start({iv: this.iv});
+    decipher.update(this.cryptForge);
+     console.log(decipher.finish());
+     console.log(decipher.output.data);
+  }
+
+  crypT(){
+    console.time('cryptoJS');
+
+    this.cryptoJs(); // run whatever needs to be timed in between the statements
+
+    console.timeEnd('cryptoJS');
+  }
+
+  decrypT(){
+    console.time('decryptoJS');
+
+    this.decryptoJs(); // run whatever needs to be timed in between the statements
+
+    console.timeEnd('decryptoJS');
+  }
+
+  crypF(){
+    console.time('cryptoForge');
+
+    this.cryptoForge(); // run whatever needs to be timed in between the statements
+
+    console.timeEnd('cryptoForge');
+  }
+
+  decrypF(){
+    console.time('decryptoForge');
+
+    this.decryptoForge(); // run whatever needs to be timed in between the statements
+
+    console.timeEnd('decryptoForge');
+  }
 }
